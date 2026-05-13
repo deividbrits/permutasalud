7ed104ef543028e0be5b8b5e7981068e8c90e545
deividbrits <dabrito.dwfs13@bootcampudd.cl>
2026-05-13 10:55:34 -0400

    Pepraracion para el despliegue en render

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ import authRoutes from "./src/routes/authRoutes";
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   // Firebase Admin is initialized via import in the header.
 
